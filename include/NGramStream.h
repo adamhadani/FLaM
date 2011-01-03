872ed798dd2030ae5604ada0d8123ee91c9d7748
Adam Ever-Hadani <adamhadani@gmail.com>
2011-01-03 08:54:19 -0800

    include dir

---
+++ include/NGramStream.h
@@ -0,0 +1,17 @@
+#ifndef NGRAMSTREAM_H
+#define NGRAMSTREAM_H
+
+namespace flam {
+
+class NGramStream
+{
+    public:
+        NGramStream();
+        virtual ~NGramStream();
+    protected:
+    private:
+};
+
+}
+
+#endif // NGRAMSTREAM_H
