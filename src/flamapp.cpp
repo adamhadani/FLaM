c9d89b018f3084f606dbd8c76f3bf19a10ef90f0
Adam Ever-Hadani <adamhadani@gmail.com>
2011-02-03 15:11:16 -0800

    added scripts dir, added wfreq2vocab

--- src/flamapp.cpp
+++ src/flamapp.cpp
@@ -36,18 +36,22 @@ class FLaMApp
 	enum TaskTypes { tBuildWordFreq, tBuildVocabulary, tBuildNGrams, tBuildIDNGrams };
 	std::map<std::string, int> taskMap;
 
-	FLaMApp() {}
+	FLaMApp(size_t wordLenThres=20) : wordLenThres(wordLenThres) {}
 	virtual ~FLaMApp() {}
 
 	int buildNGrams(const char* input_fname, const char* output_fname, int min_N=1, int max_N=3);
-	int buildVocabulary(const char* input_fname, const char* output_fname);
+	int buildIDNGrams(const char* input_fname, const char* output_fname, int min_N=1, int max_N=3);
+	int buildWFreq(const char* input_fname, const char* output_fname);
+
+  private:
+    const size_t wordLenThres;
 };
 
 /**
- * Build vocabulary from input text.
+ * Build word frequencies from input text.
  *
  */
-int FLaMApp::buildVocabulary(const char* input_fname, const char* output_fname)
+int FLaMApp::buildWFreq(const char* input_fname, const char* output_fname)
 {
 	FILE *infile=NULL, *outfile=NULL;
 	if ( !strcmp(input_fname, "-") ) {
@@ -86,6 +90,12 @@ int FLaMApp::buildVocabulary(const char* input_fname, const char* output_fname)
     HashVocabulary::SymbolMap::iterator it = hash_vocab->begin();
     for (; it != hash_vocab->end(); ++it)
     {
+        // Prune words with length above a certain threshold
+        if ( flmstrlen(it->first) > wordLenThres ) {
+            fprintf(stderr, "Skipping long word: %s\n", it->first);
+            continue;
+        }
+
         fprintf(outfile, "%i\t%s\n", it->second, it->first);
     }
 
@@ -169,11 +179,16 @@ int FLaMApp::buildNGrams(const char* input_fname, const char* output_fname, int 
 	return 0;
 }
 
+int FLaMApp::buildIDNGrams(const char* input_fname, const char* output_fname, int min_N, int max_N)
+{
+    return 0;
+}
+
 void usage()
 {
 	fprintf(stderr, "Usage: flamapp --task <task> -i <input_fname> -o <output_fname>\n"
 		"Options:\n"
-		"    --task \t Task to execute. Valid tasks are 'wfreq2vocab', 'text2wngram'.\n"
+		"    --task \t Task to execute. Valid tasks are 'text2wfreq', 'text2wngram', 'text2idngram'.\n"
 		"    -i \t Input file to read from (Use '-' for stdin).\n"
 		"    --min_N \t Used with text2wngram, minimal order of ngrams to extract.\n"
 		"    --max_N \t Used with text2wngram, maximal order of ngrams to extract.\n"
@@ -193,10 +208,12 @@ int main(int argc, char* argv[])
 	cxxtools::Arg<int> min_N(argc, argv, "--min_N", 1);
 	cxxtools::Arg<int> max_N(argc, argv, "--max_N", 3);
 
+    // Define maps enumerations. These are based on
+    // the CMU toolkit.
 	std::map<std::string, FLaMApp::TaskTypes> tasksMap;
-	//tasksMap["text2wfreq"] = FLaMApp::tBuildWordFreq;
-	tasksMap["wfreq2vocab"] = FLaMApp::tBuildVocabulary;
+	tasksMap["text2wfreq"] = FLaMApp::tBuildWordFreq;
 	tasksMap["text2wngram"] = FLaMApp::tBuildNGrams;
+	tasksMap["text2idngram"] = FLaMApp::tBuildIDNGrams;
 
 	if ( tasksMap.find(task) == tasksMap.end() || !input_fname || !output_fname ) {
 		usage();
@@ -209,12 +226,16 @@ int main(int argc, char* argv[])
 
 	switch (taskType) {
 		case FLaMApp::tBuildNGrams:
-			cerr << "Building N-Gram file. Input file: " << input_fname << ", Output file: " << output_fname << endl;
+			cerr << "Building N-Grams file. Input file: " << input_fname << ", Output file: " << output_fname << endl;
 			app.buildNGrams(input_fname, output_fname, min_N, max_N);
 			break;
-		case FLaMApp::tBuildVocabulary:
-			cerr << "Building Vocabulary file. Input file: " << input_fname << ", Output file: " << output_fname << endl;
-			app.buildVocabulary(input_fname, output_fname);
+		case FLaMApp::tBuildWordFreq:
+			cerr << "Building Word Frequencies file. Input file: " << input_fname << ", Output file: " << output_fname << endl;
+			app.buildWFreq(input_fname, output_fname);
+			break;
+		case FLaMApp::tBuildIDNGrams:
+			cerr << "Building ID N-Grams file. Input file: " << input_fname << ", Output file: " << output_fname << endl;
+			app.buildIDNGrams(input_fname, output_fname);
 			break;
 	}
 
